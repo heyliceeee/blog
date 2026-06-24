e148954bcc961cba7ea54042c4faa0f9cde81217
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2026-06-24 14:40:30 +0100

    ✨feat: blog part 1

---
+++ main.py
@@ -0,0 +1,25 @@
+import json
+from flask import Flask, render_template
+
+with open('blog-data.txt', 'r') as file:  # Open the blog data file
+    all_posts = json.load(file)  # Load the JSON data into a Python dictionary
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=all_posts) # Pass the dictionary to the template
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None # Initialize the variable
+    for blog_post in all_posts: # Iterate through the dictionary
+        if blog_post["id"] == index: # Check if the index matches the id of the blog post
+            requested_post = blog_post # If it does, assign the blog post to the variable
+    return render_template("post.html", post=requested_post)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
