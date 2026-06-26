cc1fca1ff22235031c37c56a88958640b6d3339b
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2026-06-26 17:24:01 +0100

    💄feat: contact

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import json
-from flask import Flask, render_template
+import os
+import smtplib
+from flask import Flask, render_template, request
+from email.message import EmailMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+smtp_host = os.getenv("SMTP_HOST")
+smtp_port = int(os.getenv("SMTP_PORT"))
+smtp_pass = os.getenv("SMTP_PASSWORD")
+smtp_email = os.getenv("SMTP_EMAIL")
 
 with open('blog-data.txt', 'r') as file:  # Open the blog data file
     all_posts = json.load(file)  # Load the JSON data into a Python dictionary
@@ -23,7 +34,32 @@ def show_post(index):
 def about():
     return render_template("about.html") # Render the about.html template
 
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST': # Check if the request method is POST
+        data = request.form # Get the form data
+        send_email(data['name'], data['email'], data['message']) # Call the send_email function
+        return render_template("contact.html", msg_sent=True) # Render the contact.html template with a success message
+    return render_template("contact.html", msg_sent=False) # Render the contact.html template without a success message
+
+def send_email(name, email, message):
+    """
+    Send an email using the SMTP protocol
+    :param name: name of the sender
+    :param email: email of the sender
+    :param message: message of the sender
+    """
+    msg = EmailMessage()
+    msg["Subject"] = "New Message From Your Blog"
+    msg["From"] = smtp_email
+    msg["To"] = smtp_email
+    letter = f"Name: {name}\nEmail: {email}\n\nMessage: {message}"
+    msg.set_content(letter, charset="utf-8")
+
+    with smtplib.SMTP(smtp_host, smtp_port) as conn:  # Create an SMTP connection
+        conn.starttls()  # Enable TLS encryption
+        conn.login(user=smtp_email, password=smtp_pass)  # Log in to the SMTP server
+        conn.send_message(msg)  # Send the email
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-
